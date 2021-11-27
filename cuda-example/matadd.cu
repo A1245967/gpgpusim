4ee496c67bd172711f63371329e29590bc439793
A1245967 <a1245967@gmail.com>
2021-11-27 21:21:41 +0800

    unite the variable name

--- cuda-example/matadd.cu
+++ cuda-example/matadd.cu
@@ -3,32 +3,35 @@
 #include<cuda_runtime.h>
 
 #define N 512
-#define BLOCK_SIZE 32
 
-__global__ void MatAdd(float *A, float *B, float *C){
+__global__ void MatAdd(float *A, float *B, float *C)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // C = A + B (write down your codes)
+    // C = A + B
     if(i < N && j < N){
         C[i*N+j] = A[i*N+j] + B[i*N+j];
     }
 
 }
 
-int main(){
+int main()
+{
 
     float *h_A, *h_B, *h_C;
     float *d_A, *d_B, *d_C;
 
     int i;
 
+    // allocate the host memory
     h_A = (float*)malloc(N * N * sizeof(float));
     h_B = (float*)malloc(N * N * sizeof(float));
     h_C = (float*)malloc(N * N * sizeof(float));
 
-    // init data
-    for(i = 0; i < (N * N); i++){
+    // init host data
+    for(i = 0; i < (N * N); i++)
+    {
 	    h_A[i] = 1.0;
 	    h_B[i] = 2.0;
 	    h_C[i] = 0.0;
@@ -39,37 +42,46 @@ int main(){
     cudaMalloc((void**)&d_B,  N * N * sizeof(float));
     cudaMalloc((void**)&d_C,  N * N * sizeof(float));
     
-    // transfer data to device
+    // transfer host data to device
     cudaMemcpy(d_A, h_A, N * N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, N * N * sizeof(float), cudaMemcpyHostToDevice);
 
     // fill in correct thread/blocks
-    dim3 numBlock( (N + BLOCK_SIZE - 1)/BLOCK_SIZE , (N + BLOCK_SIZE - 1)/BLOCK_SIZE);
-    dim3 threadBlock( BLOCK_SIZE, BLOCK_SIZE);
+    unsigned int x_size = 32;
+    unsigned int y_size = 32;
+    unsigned int grid_rows = (N + x_size - 1) / x_size;
+    unsigned int grid_cols = (N + y_size - 1) / y_size;
+    dim3 dimGrid(grid_cols, grid_rows);
+    dim3 dimBlock(x_size, y_size);
 
-    // MatAdd kernel
-    MatAdd<<<numBlock, threadBlock>>>(d_A, d_B, d_C);
-    cudaDeviceSynchronize();
+    // Launch MatAdd kernel
+    MatAdd<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
 
-    // transfer data back to host
+    // transfer device data back to host
     cudaMemcpy(h_C, d_C, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    for(i = 0; i < (N * N); i++){
-    	
-        if(h_C[i]!= 3.0){
-	        printf("Error:%f, idx:%d\n", h_C[i], i);
-            return 0;
+    int all_ok = 1;
+    for(i = 0; i < (N * N); i++)
+    {
+        if(h_C[i]!= 3.0)
+        {
+	        all_ok = 0;
 	    }
     }
 
-    printf("PASS\n");
+    if(all_ok)
+    {
+        printf("all results are correct!!!\n");
+    }
+    else
+    {
+        printf("incorrect results\n");
+    }
 
     // free memory
-
     free(h_A);
     free(h_B);
     free(h_C);
-
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
--- cuda-example/matmul.cu
+++ cuda-example/matmul.cu
@@ -10,29 +10,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define BLOCK_SIZE 16
-
-/*
-*********************************************************************
-function name: gpu_matrix_mult
-
-description: dot product of two matrix (not only square)
-
-parameters: 
-            &a GPU device pointer to a m X n matrix (A)
-            &b GPU device pointer to a n X k matrix (B)
-            &c GPU device output purpose pointer to a m X k matrix (C) 
-            to store the result
-
-Note:
-    grid and block should be configured as:
-        dim3 dimGrid((k + BLOCK_SIZE - 1) / BLOCK_SIZE, (m + BLOCK_SIZE - 1) / BLOCK_SIZE);
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-
-    further sppedup can be obtained by using shared memory to decrease global memory access times
-return: none
-*********************************************************************
-*/
 __global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
 { 
     int row = blockIdx.y * blockDim.y + threadIdx.y; 
@@ -48,87 +25,6 @@ __global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
     }
 } 
 
-/*
-*********************************************************************
-function name: gpu_square_matrix_mult
-
-description: dot product of two matrix (not only square) in GPU
-
-parameters: 
-            &a GPU device pointer to a n X n matrix (A)
-            &b GPU device pointer to a n X n matrix (B)
-            &c GPU device output purpose pointer to a n X n matrix (C) 
-            to store the result
-Note:
-    grid and block should be configured as:
-
-        dim3 dim_grid((n - 1) / BLOCK_SIZE + 1, (n - 1) / BLOCK_SIZE + 1, 1);
-        dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
-
-return: none
-*********************************************************************
-*/
-__global__ void gpu_square_matrix_mult(int *d_a, int *d_b, int *d_result, int n) 
-{
-    __shared__ int tile_a[BLOCK_SIZE][BLOCK_SIZE];
-    __shared__ int tile_b[BLOCK_SIZE][BLOCK_SIZE];
-
-    int row = blockIdx.y * BLOCK_SIZE + threadIdx.y;
-    int col = blockIdx.x * BLOCK_SIZE + threadIdx.x;
-    int tmp = 0;
-    int idx;
-
-    for (int sub = 0; sub < gridDim.x; ++sub) 
-    {
-        idx = row * n + sub * BLOCK_SIZE + threadIdx.x;
-        if(idx >= n*n)
-        {
-            // n may not divisible by BLOCK_SIZE
-            tile_a[threadIdx.y][threadIdx.x] = 0;
-        }
-        else
-        {
-            tile_a[threadIdx.y][threadIdx.x] = d_a[idx];
-        }
-
-        idx = (sub * BLOCK_SIZE + threadIdx.y) * n + col;
-        if(idx >= n*n)
-        {
-            tile_b[threadIdx.y][threadIdx.x] = 0;
-        }  
-        else
-        {
-            tile_b[threadIdx.y][threadIdx.x] = d_b[idx];
-        }
-        __syncthreads();
-
-        for (int k = 0; k < BLOCK_SIZE; ++k) 
-        {
-            tmp += tile_a[threadIdx.y][k] * tile_b[k][threadIdx.x];
-        }
-        __syncthreads();
-    }
-    if(row < n && col < n)
-    {
-        d_result[row * n + col] = tmp;
-    }
-}
-
-/*
-*********************************************************************
-function name: cpu_matrix_mult
-
-description: dot product of two matrix (not only square) in CPU, 
-             for validating GPU results
-
-parameters: 
-            &a CPU host pointer to a m X n matrix (A)
-            &b CPU host pointer to a n X k matrix (B)
-            &c CPU host output purpose pointer to a m X k matrix (C) 
-            to store the result
-return: none
-*********************************************************************
-*/
 void cpu_matrix_mult(int *h_a, int *h_b, int *h_result, int m, int n, int k) {
     for (int i = 0; i < m; ++i) 
     {
@@ -144,18 +40,6 @@ void cpu_matrix_mult(int *h_a, int *h_b, int *h_result, int m, int n, int k) {
     }
 }
 
-/*
-*********************************************************************
-function name: main
-
-description: test and compare
-
-parameters: 
-            none
-
-return: none
-*********************************************************************
-*/
 int main(int argc, char const *argv[])
 {
     int m, n, k;
@@ -194,23 +78,18 @@ int main(int argc, char const *argv[])
     cudaMemcpy(d_a, h_a, sizeof(int)*m*n, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, sizeof(int)*n*k, cudaMemcpyHostToDevice);
 
-    unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    unsigned int x_size = 16;
+    unsigned int y_size = 16;
+    unsigned int grid_rows = (m + x_size - 1) / x_size;
+    unsigned int grid_cols = (k + y_size - 1) / y_size;
     dim3 dimGrid(grid_cols, grid_rows);
-    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimBlock(x_size, y_size);
    
     // Launch kernel 
-    if(m == n && n == k)
-    {
-        gpu_square_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, n);    
-    }
-    else
-    {
-        gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m, n, k);    
-    }
+    gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m, n, k);
+    
     // Transefr results from device to host 
     cudaMemcpy(h_c, d_c, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
-    cudaThreadSynchronize();
 
     // start the CPU version
     cpu_matrix_mult(h_a, h_b, h_cc, m, n, k);
@@ -230,7 +109,6 @@ int main(int argc, char const *argv[])
         //printf("\n");
     }
 
-    // roughly compute speedup
     if(all_ok)
     {
         printf("all results are correct!!!\n");
